5072a663149600bdeba92a08732bac08e37987b8
xbc66666 <117884107+xbc66666@users.noreply.github.com>
2022-11-10 13:45:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9QWt5083hSDzUmrGvWlY3_Jc6-c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Gbw68zlVIpabRtVCcar8FH2vg-BRUWFPacRKVvRTG5s',
+      useTemplateId: 'OLyqcpJg_5_ntN7tZDeM8IYitHOzO3KE0ezYxv4cupU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -35,6 +35,9 @@ const USER_CONFIG = {
       
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '06-14',
+        },
+         {
+          type: '节日', name: '距离四级考试', year: '2022', date: '10-9',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
