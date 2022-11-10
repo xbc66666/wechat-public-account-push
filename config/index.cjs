2fb8353906399eb6dde7a0c9676aebb2a870088c
xbc66666 <117884107+xbc66666@users.noreply.github.com>
2022-11-10 12:12:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,96 +8,36 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxf081df0f9bde9974',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '7d93e4a6d29f197819cf52b3e0855a53',
 
-  PROVINCE: '陕西',
-  CITY: '西安',
+  PROVINCE: '广东',
+  CITY: '清远',
 
   USERS: [  
     {
-      name: '亲爱的朋友',
-      id: 'osZhI65MpjXke1V9lkHfgjpNmmjE',
-      useTemplateId: 'K8FvnxRDqTlFZtOhQzhioaaiVIAKaXdmrfT_Yp6VVCE',
-      province: '陕西',
-      city: '西安',
-      horoscopeDate: '8-19',
+      name: '小谭',
+      id: 'o9QWt58YZgHQB3RhP3H64d9lO0RE',
+      useTemplateId: 'g_eFt60aBtYsDXGYqu8ao84nvYk9Jne0egqHvJQtNls	',
+      province: '广东',
+      city: '清远',
+      horoscopeDate: '11.10',
       horoscopeDateType: '今日',
       openUrl: 'https://mp.weixin.qq.com/s?__biz=MzkwMjQwMzY0Mg==&mid=2247483682&idx=1&sn=002e49362ea8432224dd567248eb3045&chksm=c0a74277f7d0cb6192b354626eb1b374c443e112d50cf269ad437c675611e2ef3ea5e107dcd6&token=1205761133&lang=zh_CN#rd',
       festivals: [],
-      customizedDateList: [  {"type": "*生日", "name": "自己", "year": "1992", "date": "7-20"},],
+      customizedDateList: [  {"type": "*生日", "name": "小谭", "year": "2002", "date": "3-13"},],
+      customizedDateList: [  {"type": "*生日", "name": "小聪", "year": "2002", "date": "7-02"},],
       courseSchedule: null
     },
     
 
-   {
+  
 
-      name: '亲爱的朋友',
 
-      id: 'osZhI65MpjXke1V9lkHfgjpNmmjE',
-
-      useTemplateId: '3CnTPTwCffGfBC8nJw0C4TSGLtTuwVyhaj9Z1eMvruI',
-
-      province: '陕西',
-
-      city: '西安',
-
-      horoscopeDate: '8-19',
-     
-      horoscopeDateType: '今日',
-     
-      openUrl: 'https://mp.weixin.qq.com/s?__biz=MzkwMjQwMzY0Mg==&mid=2247483682&idx=1&sn=002e49362ea8432224dd567248eb3045&chksm=c0a74277f7d0cb6192b354626eb1b374c443e112d50cf269ad437c675611e2ef3ea5e107dcd6&token=1205761133&lang=zh_CN#rd',
-
-      festivals: [],
-
-      customizedDateList: [  {"type": "*生日", "name": "自己", "year": "1992", "date": "7-20"},],
-
-      courseSchedule: null
-    },  
     
 
-  {
-     name: '偷懒的小蜜蜂',
-    
-     id: 'osZhI67GIj0AsmiruHSP_-MTjJOk',
-      useTemplateId: 'K8FvnxRDqTlFZtOhQzhioaaiVIAKaXdmrfT_Yp6VVCE',
-      province: '河北',   
-      city: '石家庄',
-     horoscopeDate: '9-17',
-     horoscopeDateType: '今日',
-     openUrl: 'https://mp.weixin.qq.com/s?__biz=MzkwMjQwMzY0Mg==&mid=2247483682&idx=1&sn=002e49362ea8432224dd567248eb3045&chksm=c0a74277f7d0cb6192b354626eb1b374c443e112d50cf269ad437c675611e2ef3ea5e107dcd6&token=1205761133&lang=zh_CN#rd',
-
-      festivals: [  {"type": "*生日", "name": "father", "year": "1956", "date": "08-13"},],
-
-      customizedDateList: [  {"type": "*生日", "name": "自己", "year": "1992", "date": "7-20"},],
-
-      courseSchedule: null
-
-    },
-    
- {
-
-     name: '偷懒的小蜜蜂',
-   
-     id: 'osZhI67GIj0AsmiruHSP_-MTjJOk',
-
-      useTemplateId: '3CnTPTwCffGfBC8nJw0C4TSGLtTuwVyhaj9Z1eMvruI',
-
-      province: '河北',   
-
-      city: '石家庄',
-
-     horoscopeDate: '9-17',
-
-     horoscopeDateType: '今日',
-
-     openUrl: 'https://mp.weixin.qq.com/s?__biz=MzkwMjQwMzY0Mg==&mid=2247483682&idx=1&sn=002e49362ea8432224dd567248eb3045&chksm=c0a74277f7d0cb6192b354626eb1b374c443e112d50cf269ad437c675611e2ef3ea5e107dcd6&token=1205761133&lang=zh_CN#rd',
-      festivals: [  {"type": "*生日", "name": "father", "year": "1956", "date": "08-13"},],
-      customizedDateList: [  {"type": "*生日", "name": "自己", "year": "1992", "date": "7-20"},],
-      courseSchedule: null
-    },
   ],
 
 
