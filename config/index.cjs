c6b26b0942954c8896654dd237717014683e927d
xbc66666 <117884107+xbc66666@users.noreply.github.com>
2022-11-10 13:24:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,18 +23,16 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9QWt58YZgHQB3RhP3H64d9lO0RE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '_rvhRnZ0dDpxMX6gAnbMC80W0atOppkaGzTy4x7CWo0',
+      useTemplateId: 'posIrZsmPTTDzw3I6wyTWunUVz8nL8Y7c-tNwljfb44',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小谭', year: '2002', date: '03-13',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
+      
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '06-15',
         },
@@ -44,7 +42,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2022-06-015' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+     
       ],
     },
   ],
